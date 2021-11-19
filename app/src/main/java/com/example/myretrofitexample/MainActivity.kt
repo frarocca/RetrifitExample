804f8ef3097cf751e7effb4b08405451275107fb
Francesco <f.rocca@patente.it>
2021-11-19 10:15:42 +0100

    setOnClickListener sul bottone

--- app/src/main/java/com/example/myretrofitexample/MainActivity.kt
+++ app/src/main/java/com/example/myretrofitexample/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.myretrofitexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSetCurrentTime = findViewById<Button>(R.id.setCurrentTime)
+    // set on-click listener
+        btnSetCurrentTime.setOnClickListener {
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
