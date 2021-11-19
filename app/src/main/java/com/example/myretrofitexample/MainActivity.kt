84d28a79b51ad3d6abad102215c92d2ff8d45a02
Francesco <f.rocca@patente.it>
2021-11-19 11:52:33 +0100

    aggiunto moshi converter

--- app/src/main/java/com/example/myretrofitexample/MainActivity.kt
+++ app/src/main/java/com/example/myretrofitexample/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import com.example.myretrofitexample.Network.SimulatorApi
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +16,10 @@ class MainActivity : AppCompatActivity() {
         val btnSetCurrentTime = findViewById<Button>(R.id.setCurrentTime)
     // set on-click listener
         btnSetCurrentTime.setOnClickListener {
-            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+            CoroutineScope(IO).launch {
+                val timeResult = SimulatorApi.retrofitService.currentTime()
+                println("risultato ${timeResult.body()?.time}")
+            }
         }
 
     }
--- app/src/main/java/com/example/myretrofitexample/Network/SimulatorApiService.kt
+++ app/src/main/java/com/example/myretrofitexample/Network/SimulatorApiService.kt
@@ -1,16 +1,29 @@
 package com.example.myretrofitexample.Network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Response
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+
 
 private const val BASE_URL =
     "http://10.0.0.172:8842"
 
+
+
+// Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
+// full Kotlin compatibility.
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+
 //Retrofit needs the base URI for the web service, and a converter factory to build a web services API.
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
@@ -19,7 +32,7 @@ private val retrofit = Retrofit.Builder()
 // Response: {"time":"12.42"}
 interface SimulatorApiService {
     @GET("simuapp/currentTime")
-    fun currentTime(): Response<SimulatorTime>
+    suspend fun currentTime(): Response<SimulatorTime>
 }
 
 
