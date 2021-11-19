c342f645653c5108ba15838576d2e897d874ca77
Francesco <f.rocca@patente.it>
2021-11-19 11:14:04 +0100

    creato singleton per l'accesso all'istanza retrofit

--- app/src/main/java/com/example/myretrofitexample/Network/SimulatorApiService.kt
+++ app/src/main/java/com/example/myretrofitexample/Network/SimulatorApiService.kt
@@ -14,8 +14,20 @@ private val retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
     .build()
 
-// {"time":"12.42"}
+
+// GET "http://10.0.0.172:8842/simuapp/currentTime"
+// Response: {"time":"12.42"}
 interface SimulatorApiService {
     @GET("simuapp/currentTime")
     fun currentTime(): Response<SimulatorTime>
+}
+
+
+//object declarations to create a singleton pattern.
+//ensures that one, and only one, instance of an object is created, has one global point of access to that object.
+//Object declaration's initialization is thread-safe and done at first access.
+
+object SimulatorApi {
+    val retrofitService : SimulatorApiService by lazy {
+        retrofit.create(SimulatorApiService::class.java) }
 }
