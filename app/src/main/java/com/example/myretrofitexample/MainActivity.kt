6c26245567029723aee5d2adb53724af766f4bad
Francesco <f.rocca@patente.it>
2021-11-19 12:05:33 +0100

    stampa valore nella textView

--- app/src/main/java/com/example/myretrofitexample/MainActivity.kt
+++ app/src/main/java/com/example/myretrofitexample/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.myretrofitexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.example.myretrofitexample.Network.SimulatorApi
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,11 +18,24 @@ class MainActivity : AppCompatActivity() {
         val btnSetCurrentTime = findViewById<Button>(R.id.setCurrentTime)
     // set on-click listener
         btnSetCurrentTime.setOnClickListener {
+            setNewText("Click")
             CoroutineScope(IO).launch {
                 val timeResult = SimulatorApi.retrofitService.currentTime()
-                println("risultato ${timeResult.body()?.time}")
+                if(timeResult.isSuccessful)
+                    setTextOnMainThread("risultato ${timeResult.body()?.time}")
+                else
+                    setTextOnMainThread("errore chiamata currentTime")
             }
         }
 
+    }
+    private fun setNewText(input: String) {
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = input
+    }
+    private suspend fun setTextOnMainThread(input: String) {
+        withContext (Main) {
+            setNewText(input)
+        }
     }
 }
