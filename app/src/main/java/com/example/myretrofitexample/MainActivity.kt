e5619ddad06d203ed10cdf79e70179ddcddb6a0e
Francesco <f.rocca@patente.it>
2021-11-19 12:15:11 +0100

    configurazione pulita per l'esercitazione

--- app/src/main/java/com/example/myretrofitexample/MainActivity.kt
+++ app/src/main/java/com/example/myretrofitexample/MainActivity.kt
@@ -4,11 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
-import com.example.myretrofitexample.Network.SimulatorApi
-import kotlinx.coroutines.*
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.Dispatchers.Main
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,23 +14,11 @@ class MainActivity : AppCompatActivity() {
     // set on-click listener
         btnSetCurrentTime.setOnClickListener {
             setNewText("Click")
-            CoroutineScope(IO).launch {
-                val timeResult = SimulatorApi.retrofitService.currentTime()
-                if(timeResult.isSuccessful)
-                    setTextOnMainThread("risultato ${timeResult.body()?.time}")
-                else
-                    setTextOnMainThread("errore chiamata currentTime")
-            }
         }
 
     }
     private fun setNewText(input: String) {
         val textView = findViewById<TextView>(R.id.textView)
         textView.text = input
-    }
-    private suspend fun setTextOnMainThread(input: String) {
-        withContext (Main) {
-            setNewText(input)
-        }
     }
 }
--- app/src/main/java/com/example/myretrofitexample/Network/SimulatorTime.kt
+++
@@ -1,3 +0,0 @@
-package com.example.myretrofitexample.Network
-
-class SimulatorTime(val time: String)
